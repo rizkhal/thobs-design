3ccd8cdfcf94d9e5e9a433fe140d1dd842c1fdf1
Rizkhal Lamaau <lamaaurizkhal@gmail.com>
2020-07-16 02:03:27 +0800

    re routing :hammer:

--- resources/views/layouts/back/partials/sidebar.blade.php
+++ resources/views/layouts/back/partials/sidebar.blade.php
@@ -4,36 +4,36 @@
         <nav>
             <ul class="nav">
                 <li>
-                    <a href="{{ route('admin.index') }}" class="{{ active('home') }}"><i class="lnr lnr-home"></i> <span>Dashboard</span></a>
+                    <a href="{{ route('dashboard') }}" class="{{ active('dashboard') }}"><i class="lnr lnr-home"></i> <span>Dashboard</span></a>
                 </li>
                 <li>
-                    <a href="{{ route('admin.blog.index') }}" class="{{ active('blog*') }}"><i class="lnr lnr-bullhorn"></i> <span>Blog</span></a>
+                    <a href="{{ route('admin.blog.index') }}" class="{{ active('manage/blog*') }}"><i class="lnr lnr-bullhorn"></i> <span>Blog</span></a>
                 </li>
                 <li>
-                    <a href="{{ route('admin.projects.index') }}" class="{{ active('projects*') }}"><i class="lnr lnr-store"></i> <span>Project</span></a>
+                    <a href="{{ route('admin.projects.index') }}" class="{{ active('manage/projects*') }}"><i class="lnr lnr-store"></i> <span>Project</span></a>
                 </li>
                 <li>
-                    <a href="#setting" data-toggle="collapse" class="{{ active('setting*') }}">
+                    <a href="#setting" data-toggle="collapse" class="{{ active('manage/setting*') }}">
                         <i class="lnr lnr-cog"></i><span>Setting</span>
                         <i class="icon-submenu lnr lnr-chevron-left"></i>
                     </a>
-                    <div id="setting" class="collapse {{ active('setting*') }}">
+                    <div id="setting" class="collapse {{ active('manage/setting*') }}">
                         <ul class="nav">
                             <li>
-                                <a href="{{ route('admin.setting.social.index') }}" class="{{ active('setting/social*') }}"><i class="lnr lnr-star-half"></i> <span>Social Media</span></a>
+                                <a href="{{ route('admin.setting.social.index') }}" class="{{ active('manage/setting/social*') }}"><i class="lnr lnr-star-half"></i> <span>Social Media</span></a>
                             </li>
                         </ul>
                     </div>
                 </li>
                 <li>
-                    <a href="#master" data-toggle="collapse" class="{{ active('category') }}">
+                    <a href="#master" data-toggle="collapse" class="{{ active('manage/category') }}">
                         <i class="lnr lnr-database"></i><span>Data Master</span>
                         <i class="icon-submenu lnr lnr-chevron-left"></i>
                     </a>
-                    <div id="master" class="collapse {{ active('category') }}">
+                    <div id="master" class="collapse {{ active('manage/category') }}">
                         <ul class="nav">
                             <li>
-                                <a href="{{ route('admin.category.index') }}" class="{{ active('category') }}"><i class="lnr lnr-list"></i> <span>Category</span></a>
+                                <a href="{{ route('admin.category.index') }}" class="{{ active('manage/category') }}"><i class="lnr lnr-list"></i> <span>Category</span></a>
                             </li>
                         </ul>
                     </div>
--- routes/web.php
+++ routes/web.php
@@ -1,48 +1,51 @@
 <?php
 
-use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
+/** Auth */
 Auth::routes();
 
 /** Laravel File Manager */
 Route::group(['prefix' => 'laravel-filemanager', 'middleware' => ['web', 'auth']], function () {
-     \UniSharp\LaravelFilemanager\Lfm::routes();
- });
-
-Route::middleware('auth')->as('admin.')->group(function() {
-	Route::get('home', 'HomeController@index')->name('index');
-
-	/** Setting */
-	Route::prefix('setting')->as('setting.')->group(function() {
-		Route::resource('social', 'SocialMediaController');
-	});
-
-	/** Category */
-	Route::resource('category', 'CategoryController');
-	Route::prefix('select2')->as('select2.')->group(function() {
-		Route::get('category', 'Service\Select2Controller@category')->name('category');
-	});
-
-	/** Upload facade */
-	Route::prefix('uploads')->as('upload.')->group(function() {
-		Route::post('project', 'Service\UploadController@project')->name('project');
-	});
-
-	/** projects */
-	Route::resource('projects', 'ProjectController')->except('show');
-	Route::prefix('projects')->as('projects.')->group(function() {
-		Route::post('update-status', 'ProjectController@status')->name('status');
-	});
-
-	/** blog */
-	Route::resource('blog', 'BlogController');
-	Route::prefix('blog')->as('blog.')->group(function() {
-		Route::post('update-status', 'BlogController@status')->name('status');
-	});
+    \UniSharp\LaravelFilemanager\Lfm::routes();
 });
 
-Route::as('application.')->group(function() {
-	Route::get('/', 'ApplicationController@index')->name('index');
-	Route::get('project', 'ApplicationController@project')->name('project');
+/** Dashboard */
+Route::get('dashboard', 'HomeController@dashboard')->name('dashboard');
+
+/** Admin Manage Group */
+Route::prefix('manage')->as('admin.')->middleware('auth')->group(function () {
+    /** Setting */
+    Route::prefix('setting')->as('setting.')->group(function () {
+        Route::resource('social', 'SocialMediaController');
+    });
+
+    /** Category */
+    Route::resource('category', 'CategoryController');
+    Route::prefix('select2')->as('select2.')->group(function () {
+        Route::get('category', 'Service\Select2Controller@category')->name('category');
+    });
+
+    /** Upload facade */
+    Route::prefix('uploads')->as('upload.')->group(function () {
+        Route::post('project', 'Service\UploadController@project')->name('project');
+    });
+
+    /** Project */
+    Route::resource('projects', 'ProjectController')->except('show');
+    Route::prefix('projects')->as('projects.')->group(function () {
+        Route::post('update-status', 'ProjectController@status')->name('status');
+    });
+
+    /** Blog */
+    Route::resource('blog', 'BlogController');
+    Route::prefix('blog')->as('blog.')->group(function () {
+        Route::post('update-status', 'BlogController@status')->name('status');
+    });
+});
+
+/** Application */
+Route::as('application.')->group(function () {
+    Route::get('/', 'ApplicationController@index')->name('index');
+    Route::get('project', 'ApplicationController@project')->name('project');
 });
