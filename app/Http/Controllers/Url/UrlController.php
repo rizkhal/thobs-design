c2169e522e267745bdb8ba2aed22f691e20f8bac
Rizkhal Lamaau <lamaaurizkhal@gmail.com>
2020-07-22 04:05:11 +0800

    fix stored shorten urls :wrench:

--- app/Http/Controllers/Url/UrlController.php
+++ app/Http/Controllers/Url/UrlController.php
@@ -4,10 +4,18 @@ namespace App\Http\Controllers\Url;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\UrlRequest;
+use App\Repository\Shortener\UrlRepo;
 use Illuminate\Http\Request;
 
 class UrlController extends Controller
 {
+    protected $urlRepo;
+
+    public function __construct(UrlRepo $url)
+    {
+        $this->urlRepo = $url;
+    }
+
     public function index()
     {
         return view('backend::shortener.index');
@@ -20,6 +28,11 @@ class UrlController extends Controller
 
     public function store(UrlRequest $request)
     {
-        dd($request->data());
+        if ($this->urlRepo->shortenUrl(logged_in_user()->id, $request->data())) {
+            notice('success', 'Successfully shortened the url');
+            return redirect()->back();
+        }
+
+        ddd('aa');
     }
 }
---
+++ app/Repository/Shortener/UrlRepo.php
@@ -0,0 +1,19 @@
+<?php
+
+declare (strict_types = 1);
+
+namespace App\Repository\Shortener;
+
+if (!interface_exists('UrlRepo')) {
+    interface UrlRepo
+    {
+        /**
+         * Shorten of the url
+         * 
+         * @param  string $id
+         * @param  array  $data
+         * @return object
+         */
+        public function shortenUrl(string $id, array $data): object;
+    }
+}
